a82608c5849ac8692313599cef9fbea102ba3a44
shravan77 <sharvanrathaud7797@gmail.com>
2024-01-28 14:01:12 +0530

    lecture 2

--- Lecture-1/index.js
+++ Lecture-1/index.js
@@ -1,12 +1,27 @@
 
 const express = require("express")
-const app =  express()
+const app = express()
 
 app.get("/", (req, res) => {
     console.log("request recieved");
-    res.send("all okay")
+    return res.send("all okay")
 })
 
-app.listen(8000,() => {
+//query
+//api?key=val
+app.get("/api", (req, res) => {
+    console.log(req.query);
+    console.log(req.query.key);
+    return res.send("query api")
+})
+
+//params
+app.get("/joke/:id/:id2", (req, res) => {
+    console.log(req.params);
+    console.log(req.params.id);
+    return res.send(`params ${req.params.id}, ${req.params.id2}`)
+})
+
+app.listen(8000, () => {
     console.log("server running 8000");
 })
